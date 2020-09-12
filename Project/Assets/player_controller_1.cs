acc0687646bb0628d48f7331deff05a09724ee9a
vausq <vascoboucher22@gmail.com>
2020-09-12 20:10:53 -0300

    1.1.1 update
    patol pattern for tscp 039 and controlls n' animation for researcher

---
+++ Project/Assets/player_controller_1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller_1 : MonoBehaviour{
+
+    public Animator animator;
+    public float speed=2.0f;
+
+    void Update(){
+        
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + move * Time.deltaTime * speed;
+
+    }
+}
---
+++ Project/Assets/scp039_patrol.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scp039_patrol : MonoBehaviour
+{
+    //variables changeable in unity
+    public float speed=2.0f;
+    public Transform[] movespots;
+    public float waittime;
+     
+    //fixed within code
+    private float startwaittime = 0.5f;
+    private int randomspot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomspot = Random.Range(0, movespots.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, movespots[randomspot].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, movespots[randomspot].position)<0.2f){
+            if(waittime <= 0){
+                randomspot = Random.Range(0, movespots.Length);
+                waittime = startwaittime;
+            }   else {waittime -= Time.deltaTime;}
+
+
+        }
+    
+    }
+}
